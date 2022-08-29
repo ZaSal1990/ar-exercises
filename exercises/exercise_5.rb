47a76a4e3f35fed3d422ff91f59ee4e307bbc33d
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-08-29 14:17:39 -0600

    add employees to store

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,7 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts sum = "Annual Revenues: #{Store.sum(:annual_revenue)}"
+puts average = "Average Revenue: #{Store.average(:annual_revenue)}"
+puts highest_grossing_store = "Highest Grosssing Stores #{Store.where("annual_revenue > '1000000'").count}"
+
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Zahra", last_name: "Salman", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 40)
+@store1.employees.create(first_name: "Salman", last_name: "Akhtar", hourly_rate: 60)
+@store2.employees.create(first_name: "Denille", last_name: "Gracia", hourly_rate: 40)
+@store2.employees.create(first_name: "John", last_name: "Hopkins", hourly_rate: 50)
+@store2.employees.create(first_name: "Sonam", last_name: "Kapoor", hourly_rate: 50)
+
+
+puts @store1.employees.count
+puts @store2.employees.count
