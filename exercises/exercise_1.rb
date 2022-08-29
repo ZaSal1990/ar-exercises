ee748c39d635d4e39b9867d4c650421a3d0762bb
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-08-29 13:55:00 -0600

    instanciate stores and retreive stores based on conditions

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,7 +5,7 @@ puts "----------"
 
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: 1, womens_apparel: 1)
 Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: 0, womens_apparel: 1)
-Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: 0, womens_apparel: 0)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: 1, womens_apparel: 0)
 
 puts Store.count
 
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,19 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: 0, womens_apparel: 1)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: 1, womens_apparel: 0)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: 1, womens_apparel: 1)
+
+puts Store.count
+
+@mens_store = Store.where("mens_apparel = '1' and womens_apparel = '0'")
+
+@mens_store.each do |store_name|
+  puts "#{store_name.name}: #{store_name.annual_revenue}"
+end
+
+@womens_store = Store.where("mens_apparel = '0' and womens_apparel = '1' and annual_revenue < '1000000'")
+@womens_store.each do |store_name|
+  puts "#{store_name.name}: #{store_name.annual_revenue}"
+end
