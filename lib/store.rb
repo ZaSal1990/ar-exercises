03cc984a37c3b2864e577e6337bcf269980226e9
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-08-29 15:53:09 -0600

    add validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality : { only_integer: true }
+  validate :carries_either_stock
 end
+
+  def carries_either_stock
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:mens_apparel, "at least one kind of apparel must be carried")
+    end
+  end
